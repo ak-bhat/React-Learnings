3fa3480aab4c001693306d6cd7237be6f7ffb2e6
A.K Bhat <g.ananthakrishnabhat@gmail.com>
2024-11-14 19:04:40 +0530

    1st episode

---
+++ README.md
@@ -0,0 +1,1 @@
+# Namaste React - Exploring the React with Akshay Saini 
---
+++ app.js
@@ -0,0 +1,12 @@
+const heading = React.createElement(
+    "div", 
+    {id: "parent"}, 
+    React.createElement(
+        "div",
+        {id:"child"},[
+        React.createElement("h1",{},"Hi I'm the h1 tag"),
+        React.createElement("h2",{},"Hi I'm the h2 tag")
+        ])
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading)
