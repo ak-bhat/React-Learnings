01ec394e151599a6ef01933954a3dcf1ca72f334
A.K Bhat <g.ananthakrishnabhat@gmail.com>
2024-11-19 11:41:08 +0530

    class state variables

--- src/components/User.js
+++ src/components/User.js
@@ -1,8 +1,12 @@
+import { useState } from "react";
+
 const User = ({name, location}) => {
+    const [count, setCount] = useState(0)
     return(
         <div className="user-card">
             <h3>Name:{name}</h3>
             <h3>Name:{location}</h3>
+            <h2>Count:{count}</h2>
         </div>
     )
 }
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -2,14 +2,22 @@ import React from "react"
 
 class UserClass extends React.Component{
     constructor(props){
-        super(props)
+        super(props);
+
+        this.state = {
+            count: 0,
+            count2: 2
+        }
     }
     render(){
         const {name, location} = this.props
+        const {count, count2} = this.state
         return(
             <div className="user-card">
                 <h3>Name:{name}</h3>
                 <h3>Name:{location}</h3>
+                <h2>Count:{count}</h2>
+                <h2>Count:{count2}</h2>
             </div>
         )
     }
