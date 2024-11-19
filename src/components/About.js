99fe1a53e4a55257087e2020f30e1891a7c01e20
A.K Bhat <g.ananthakrishnabhat@gmail.com>
2024-11-19 11:11:44 +0530

    class component added

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,12 @@
+import User from "./User"
+import UserClass from "./UserClass"
+
 const About = () =>{
     return(
         <div className="about-body">
             <h1>About Page</h1>
+            <User name={"Ananthu"} location={"Kochi"}/>
+            <UserClass name={"Ananthu"} location={"Kochi"}/>
         </div>
     )
 }
---
+++ src/components/User.js
@@ -0,0 +1,10 @@
+const User = ({name, location}) => {
+    return(
+        <div className="user-card">
+            <h3>Name:{name}</h3>
+            <h3>Name:{location}</h3>
+        </div>
+    )
+}
+
+export default User;
