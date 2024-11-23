b0efe1d9406d60fe264620ae5fb0a2ca6f2804ef
A.K Bhat <g.ananthakrishnabhat@gmail.com>
2024-11-23 20:39:09 +0530

    Higher Order Added

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,69 +1,92 @@
-import RestaurantCard from "./RestaurantCard"
-import { useEffect, useState } from "react"
-import { Link } from "react-router-dom"
-import useOnlineStatus from "../utils/useOnlineStatus"
+import RestaurantCard, {withPromotedLabel} from "./RestaurantCard";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Body = () => {
+  const [listRestaurants, setListRestaurants] = useState([]);
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
 
-    const [listRestaurants, setListRestaurants] = useState([])
-    const [filteredRestaurants, setFilteredRestaurants] = useState([])
-    
+  const [searchText, setSearchText] = useState("");
 
-    const[searchText, setSearchText] = useState("")
+  const RestaurantCardPromoted = withPromotedLabel(RestaurantCard);
 
-    useEffect(()=>{
-        fetchData();
-    },[]);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    const fetchData = async () =>{
-        const response = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=9.91850&lng=76.25580&collection=80424&tags=layout_CCS_Dosa&sortBy=&filters=&type=rcv2&offset=0&page_type=null")
+  const fetchData = async () => {
+    const response = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=9.91850&lng=76.25580&collection=80424&tags=layout_CCS_Dosa&sortBy=&filters=&type=rcv2&offset=0&page_type=null"
+    );
 
-        const data = await response.json()
-        console.log(data.data.cards.slice(3, 16).map(card => card.card.card.info))
-        setListRestaurants(data.data.cards.slice(3,16).map(card => card.card.card.info))
-        setFilteredRestaurants(data.data.cards.slice(3,16).map(card => card.card.card.info))
-    }
+    const data = await response.json();
+    console.log(
+      data.data.cards.slice(3, 16).map((card) => card.card.card.info)
+    );
+    setListRestaurants(
+      data.data.cards.slice(3, 16).map((card) => card.card.card.info)
+    );
+    setFilteredRestaurants(
+      data.data.cards.slice(3, 16).map((card) => card.card.card.info)
+    );
+  };
 
-    const onlineStatus = useOnlineStatus();
+  const onlineStatus = useOnlineStatus();
 
+  if (onlineStatus === false) return <h1>Please check your Network</h1>;
 
-    if(onlineStatus===false)return (<h1>Please check your Network</h1>);
-
-
-    return (
-        <div className="body">
-            <div className="filter flex">
-                <div className="m-4">
-                    <input type="text" className="border border-solid border-black" value={searchText} 
-                    onChange={
-                        (e)=>{setSearchText(e.target.value)}}>
-                        </input>
-                    <button className="px-4 py-2 bg-green-100 m-4 rounded-lg" onClick={()=>{
-                            const filteredRes = listRestaurants.filter(resName=>
-                                resName.name.toLowerCase().includes(searchText.toLowerCase()))
-                                setFilteredRestaurants(filteredRes)
-                            
-                        }}>Search</button>
-                </div>
-                <div className="m-4 p-4"> 
-                    <button className="px-2 p-2 bg-gray-100 rounded-lg" 
-                        onClick={ () =>{
-                            const filteredData = listRestaurants.filter(res=> res.avgRating > 4.5);
-                            setListRestaurants(filteredData);
-                        }}>Top Rated Restaurants</button>
-                </div>
-                
-            </div>
-                
-            <div className="flex flex-wrap rounded-lg">
-                {filteredRestaurants.map(restaurant =>
-                    <Link to={"/restaurants/"+restaurant.id}><RestaurantCard key={restaurant.id} resData={restaurant}/></Link>
-                )}      
-            </div>
+  return (
+    <div className="body">
+      <div className="filter flex">
+        <div className="m-4">
+          <input
+            type="text"
+            className="border border-solid border-black"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          ></input>
+          <button
+            className="px-4 py-2 bg-green-100 m-4 rounded-lg"
+            onClick={() => {
+              const filteredRes = listRestaurants.filter((resName) =>
+                resName.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+              setFilteredRestaurants(filteredRes);
+            }}
+          >
+            Search
+          </button>
         </div>
-    )
-    
+        <div className="m-4 p-4">
+          <button
+            className="px-2 p-2 bg-gray-100 rounded-lg"
+            onClick={() => {
+              const filteredData = listRestaurants.filter(
+                (res) => res.avgRating > 4.5
+              );
+              setListRestaurants(filteredData);
+            }}
+          >
+            Top Rated Restaurants
+          </button>
+        </div>
+      </div>
 
-}
+      <div className="flex flex-wrap rounded-lg">
+        {filteredRestaurants.map((restaurant) => (
+          <Link to={"/restaurants/" + restaurant.id}>
+            {
+                restaurant.promoted === true ? <RestaurantCardPromoted resData={restaurant}/> : <RestaurantCard key={restaurant.id} resData={restaurant} />
 
-export default Body
+            }
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
--- src/components/Error.js
+++ src/components/Error.js
@@ -5,7 +5,7 @@ const Error = () =>{
     const error = useRouteError();
     return(
         <div>
-            <h1>Oops!</h1>
+            <h1 className="text-lg">Oops!</h1>
             <h2>An Error Occured!</h2>
             <h3>{error.status}:{error.statusText}</h3>
         </div>
