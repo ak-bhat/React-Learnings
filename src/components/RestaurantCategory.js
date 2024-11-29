6306ee94d10f0e063577761bfadac068630884a3
A.K Bhat <g.ananthakrishnabhat@gmail.com>
2024-11-29 21:51:08 +0530

    Accordian added with toggle feature for categories in Menu

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,16 +1,26 @@
+import { useState } from "react";
 import ItemList from "./ItemList";
 
 const RestaurantCategory = ({data}) => {
-console.log(data);
+// console.log(data);
+
+    const [showItems, setShowItems] = useState(false)
+
+    const handleClick = () =>{
+        setShowItems(!showItems)
+        //Toggle feature with Same logic - showItems?setShowItems(false):setShowItems(true)
+    }
+    // console.log(showItems);
+    
 
     return(
         <div>
-            <div className="w-6/12 mx-auto my-4 bg-gray-50 shadow-lg p-4 text-lg font-bold">
-            <div className="flex justify-between">
+            <div className="w-6/12 mx-auto my-5 bg-gray-100 shadow-lg p-4 text-lg ">
+            <div className="flex justify-between font-bold cursor-pointer" onClick={handleClick}>
                 <span>{data.title} ({data.itemCards.length}) </span>
-                <span>🔽</span>
+                <span>{showItems?"🔼":"🔽"}</span>
             </div>
-                <ItemList items={data.itemCards}/>
+                {showItems && <ItemList items={data.itemCards}/>}
             </div>
         </div>
     )
