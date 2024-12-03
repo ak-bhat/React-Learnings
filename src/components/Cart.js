9799723b5f9b82843a92dd451df1cbacf2566205
A.K Bhat <g.ananthakrishnabhat@gmail.com>
2024-12-03 11:16:32 +0530

    cart css changed

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import { clearCart, removeItem } from "../utils/cartSlice";
+import ItemList from "./ItemList";
 
 const Cart = () => {
   const items = useSelector((store) => store.cart.items);
@@ -11,44 +12,25 @@ const Cart = () => {
     console.log(index);
   };
 
-  const removeCart = () =>{
+  const removeCart = () => {
     dispatch(clearCart());
-  }
+  };
 
-  return items.length !== 0 ? (
-    <div>
-      <div>
-        {items.map((item, index) => (
-          <div
-            key={item.card.info.id}
-            className="p-2 m-2 border-b-2 border-gray-200 text-left flex justify-between"
-          >
-            <div className="w-9/12">
-              <div className="py-2">
-                <span>{item.card.info.name}</span>
-              </div>
-              <span>
-                ₹
-                {item.card.info.price
-                  ? item.card.info.price / 100
-                  : item.card.info.defaultPrice / 100}
-              </span>
-              <p className="text-xs py-2">{item.card.info.description}</p>
-            </div>
-            <div className="w-3/12 p-4 ">
-              <button
-                className="p-2 mx-9 text-red-500 rounded-lg bg-white shadow-lg"
-                onClick={() => handleitem(index)}
-              >
-                Remove
-              </button>
-            </div>
-          </div>
-        ))}
+  return items.length === 0 ? (
+    <div className="text-center m-4 p-4">
+      <h1 className="text-2xl font-bold">Cart is Empty</h1>
+    </div>
+  ) : (
+    <div className="text-center m-4 p-4">
+      <h1 className="text-2xl font-bold">Cart</h1>
+      <div className="w-6/12 m-auto">
+        <ItemList items={items} />
       </div>
-      <div className="flex justify-between items-center p-4">
-        <button className="p-2 mx-9 text-white rounded-lg bg-red-700 shadow-lg "
-        onClick={removeCart}>
+      <div className="items-center p-4">
+        <button
+          className="p-2 mx-9 text-white rounded-lg bg-red-700 shadow-lg "
+          onClick={removeCart}
+        >
           Clear Cart
         </button>
         <button className="p-2 mx-9 text-white rounded-lg bg-green-700 shadow-lg ml-auto">
@@ -56,11 +38,23 @@ const Cart = () => {
         </button>
       </div>
     </div>
-  ) : (
-    <div>
-      <h1>Cart is Empty</h1>
-    </div>
   );
+  //   return items.length !== 0 ? (
+  //     <div>
+
+  //       <button
+  //                 className="p-2 mx-9 text-red-500 rounded-lg bg-white shadow-lg"
+  //                 onClick={() => handleitem(index)}
+  //               >
+  //                 Remove
+  //               </button>
+  //     </div>
+
+  //   ) : (
+  //     <div>
+  //       <h1>Cart is Empty</h1>
+  //     </div>
+  //   );
 };
 
 export default Cart;
